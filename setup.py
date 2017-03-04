265ffddb61dd4f6299267ff6eebe6ab6aadd5f75
sudiptatung <sudiptatung@gmail.com>
2017-03-04 18:36:51 +0530

    minor

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='effectsizecalc',
-      version='3.1',
+      version='3.2',
       description='Tool for calculating effect size between group means',
       url='https://github.com/sudiptatung/EffectSizeCalculator.git',
       author='Sudipta Tung',
